c5f7528cf703e3db4ca71e5cd7d23466b0031496
paddy meade <patrickmeade4@gmail.com>
2023-02-17 22:40:37 +0000

    hook up mongodb atlas database

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://patrickmeade4:<password>@cluster0.lac0cfm.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
