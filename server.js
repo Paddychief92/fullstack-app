316dc5e3136bd86db99d2f362090809da1cbea01
paddy meade <patrickmeade4@gmail.com>
2023-02-22 18:59:02 +0000

    fixed back end create front end

--- server.js
+++ server.js
@@ -6,11 +6,29 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://patrickmeade4:DvXduHQBohYJy63B@cluster0.lac0cfm.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch((err) => {
+    console.log('Error: ' + err);
+  });
+
+
+  app.post('/users', (req, res) => {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    user.save((err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send('User created successfully');
+      }
+    });
+  });
+  
+  
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
