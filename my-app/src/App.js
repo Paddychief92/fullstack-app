16b30442a1c258aa55432fe936f8d6be738b1b3e
paddy meade <patrickmeade4@gmail.com>
2023-02-23 22:19:10 +0000

    got react route set up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // Importing the App's stylesheet from the local file system
+import React, { useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-function App() {
+function WelcomePage(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to the game, {props.username}!</h1>
     </div>
   );
 }
 
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:3000', { username, password })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Add a new user</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <h1>Add a new user</h1>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+                </div>
+                <div>
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                </div>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          }/>
+          <Route path="/welcome" element={<WelcomePage username={username} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
 export default App;
+
--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const uri = "mongodb+srv://patrickmeade4:DvXduHQBohYJy63B@cluster0.lac0cfm.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// Create a mongoose schema for the user collection
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+// Create a mongoose model for the user collection using the user schema
+const User = mongoose.model('User', userSchema);
+
+// Connect to MongoDB Atlas database
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB Connected');
@@ -15,25 +32,28 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('Error: ' + err);
   });
 
-
-  app.post('/users', (req, res) => {
-    const { username, password } = req.body;
-    const user = new User({ username, password });
-    user.save((err) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send('User created successfully');
-      }
-    });
-  });
-  
-  
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Set up a POST route to handle new user creation or password updates
+app.post('/', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    let user = await User.findOne({ username }); // find the user in the database by username
+    if (!user) { // if user not found, create a new one
+      user = new User({
+        username,
+        password
+      });
+    } else { // if user found, update the password for the found user
+      user.password = password;
+    }
+    await user.save(); // save the updated user or new user in the database
+    res.send(`User ${username} ${user.isNew ? 'created' : 'updated'} with new password`);
+  } catch (error) {
+    console.log(error);
+    res.send('Error creating/updating user');
+  }
 });
 
+// Start the server listening on port 3000
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
